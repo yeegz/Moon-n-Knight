3f520da87601c143d90a082d12602b8789b74abb
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-08-29 18:00:41 +0800

    add 8 directional movement

--- level_1/scripts/playertemp.gd
+++ level_1/scripts/playertemp.gd
@@ -1,14 +1,24 @@
 extends CharacterBody2D
+class_name player
+
+var speed = 2
+var direction = 1
 
 func _process(delta):
-	movement()
+	movement_up()
+	movement_side()
 
-func movement():
+
+func movement_up():
 	if Input.is_action_pressed("ui_up"):
 		position.y -= 2
 	elif Input.is_action_pressed("ui_down"):
 		position.y += 2
-	elif Input.is_action_pressed("ui_left"):
-		position.x -= 2
+	
+	move_and_slide()
+
+func movement_side():
+	if Input.is_action_pressed("ui_left"):
+		position.x += speed*(-direction)
 	elif Input.is_action_pressed("ui_right"):
-		position.x += 2
+		position.x += speed*(direction)
