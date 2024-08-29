3143b2aa8a86649cb37b7b0d5d53310611124934
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-08-29 18:23:25 +0800

    implement animation in player

--- level_1/scripts/playertemp.gd
+++ level_1/scripts/playertemp.gd
@@ -3,20 +3,31 @@ class_name player
 
 var speed = 2
 var direction = 1
+@onready var animation = $AnimatedSprite2D
 
 func _process(delta):
 	movement_up()
 	movement_side()
+	animations()
 
 
 func movement_up():
 	if Input.is_action_pressed("ui_up"):
-		position.y -= 2
+		position.y -= speed
 	elif Input.is_action_pressed("ui_down"):
-		position.y += 2
+		position.y += speed
 
 func movement_side():
 	if Input.is_action_pressed("ui_left"):
 		position.x += speed*(-direction)
 	elif Input.is_action_pressed("ui_right"):
 		position.x += speed*(direction)
+
+func animations():
+	if Input.is_action_just_pressed("ui_left"):
+		animation.flip_h = true
+	elif Input.is_action_just_pressed("ui_right"):
+		animation.flip_h = false
+	
+	if Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right") or Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down"):
+		animation.play("walking")
